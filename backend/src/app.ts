9b2963d1483943353470eb89c51346e74ae232a2
Emmanuel <okoosiemmanuel@gmail.com>
2026-09-12 22:56:37 +0100

    feat: add user retrieval and creation endpoints to the API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { Application } from "express";
 import cors from "cors";
 import { env } from "./config/env.js";
+import { prisma } from "./lib/prisma.js";
 
 const app: Application = express();
 
@@ -14,6 +15,32 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Server is running successfully" });
+});
+
+app.get("/users", async(req, res) => {
+    const users = await prisma.user.findMany();
+    res.status(200).json({ 
+        success: true,
+        message: "Users endpoint",
+        data: users
+    });
+})
+
+app.post("/users", async(req, res) => {
+    const { name, email, password } = req.body;
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password
+        }
+    });
+    
+    res.status(201).json({ 
+        success: true,
+        message: "User created successfully",
+        data: user
+    });
 });
 
 const PORT = env.PORT || 5000;
