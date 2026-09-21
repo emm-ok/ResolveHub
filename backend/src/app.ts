a3810ebb5b29f3686136a29cd23972dbae91b027
Emmanuel <okoosiemmanuel@gmail.com>
2026-09-21 23:22:37 +0100

    refactor: reorganize app.ts by adding route handlers and middleware for webhooks and users

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,10 @@ import cors from "cors";
 import { env } from "./config/env.js";
 import { prisma } from "./lib/prisma.js";
 
+import webhookRoutes from "./routes/webhook.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import { clerkMiddleware } from "@clerk/express";
+
 const app: Application = express();
 
 app.use(cors({
@@ -11,36 +15,28 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use("/api/webhooks", webhookRoutes)
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(clerkMiddleware())
+
+
+app.use("/api/users", userRoutes);
+
+// app.get("/users", async(req, res) => {
+//     const users = await prisma.user.findMany();
+//     res.status(200).json({ 
+//         success: true,
+//         message: "Users endpoint",
+//         data: users
+//     });
+// })
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Server is running successfully" });
-});
-
-app.get("/users", async(req, res) => {
-    const users = await prisma.user.findMany();
-    res.status(200).json({ 
-        success: true,
-        message: "Users endpoint",
-        data: users
-    });
-})
-
-app.post("/users", async(req, res) => {
-    const { name, email, password } = req.body;
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            password
-        }
-    });
-    
-    res.status(201).json({ 
-        success: true,
-        message: "User created successfully",
-        data: user
-    });
 });
 
 const PORT = env.PORT || 5000;
